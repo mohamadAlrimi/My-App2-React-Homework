e4d8b23f7cff1959acf6c7d80f84c7aa2add44e3
mohamadAlrimi <117595968+mohamadAlrimi@users.noreply.github.com>
2025-01-22 23:33:41 +0300

    i made the header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+import MyHeader from './Component/Header';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-   
-    </div>
+   <>
+   <MyHeader/>
+   </>
   );
 }
 
---
+++ src/Component/Header.js
@@ -0,0 +1,8 @@
+import "./Header.css"
+export default function MyHeader(){
+    return(
+        <>
+        <h1 className={"activ"}>Elrimi Academy</h1>
+        </>
+    )
+}
