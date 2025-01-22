a15ed3cf2b9193c8768187512ec3b7deaa30ce2c
mohamadAlrimi <117595968+mohamadAlrimi@users.noreply.github.com>
2025-01-23 00:01:05 +0300

    the post section done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import MyHeader from './Component/Header';
+import MyPosts from './Component/Post';
 import './App.css';
 
 function App() {
   return (
    <>
    <MyHeader/>
+   <MyPosts/>
+   <MyPosts/>
+   <MyPosts/>
+   <MyPosts/>
    </>
   );
 }
---
+++ src/Component/Post.js
@@ -0,0 +1,12 @@
+import "./Post.css"
+export default function MyPosts(){
+    return(
+        <>
+        <div className="post">
+            <h3 className="post1">This is the post Title</h3>
+            <hr/>
+            <p>this is the post body</p>
+        </div>
+        </>
+    )
+}
