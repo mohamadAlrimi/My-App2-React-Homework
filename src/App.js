1c0a552acd06ce3c0cc033a60df13cb2117b8c91
mohamadAlrimi <117595968+mohamadAlrimi@users.noreply.github.com>
2025-01-23 18:57:54 +0300

    muhammed elrimi header

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import MyHeader from './Component/Header';
 import MyPosts from './Component/Post';
 import MyButton from './Component/Button';
+import Header1 from './Component/Header1';
 import './App.css';
 
 function App() {
   return (
    <>
+   
    <MyHeader/>
    <MyPosts/>
    <MyPosts/>
    <MyPosts/>
    <MyPosts/>
    <MyButton/>
-  
+   <Header1/>
    </>
   );
 }
---
+++ src/Component/Header1.js
@@ -0,0 +1,8 @@
+import "./Header1.css"
+export default function Header1 (){
+    return(
+        <>
+        <h1 className="h">Muhammed Elrimi</h1>
+        </>
+    )
+}
